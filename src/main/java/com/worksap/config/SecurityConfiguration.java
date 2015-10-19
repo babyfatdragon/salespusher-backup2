d5af01356033005587411366654f9a06c0099040
LiZhenshuo <li_zh@worksap.co.jp>
2015-10-19 21:19:13 +0800

    start development. add login & logout function for default user

---
+++ src/main/java/com/worksap/config/SecurityConfiguration.java
@@ -0,0 +1,32 @@
+package com.worksap.config;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
+
+import com.worksap.CsrfHeaderFilter;
+
+@Configuration
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER) 
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+		.httpBasic().and()
+		.authorizeRequests()
+		.antMatchers("/index.html", "/partials/_home.html", "/partials/_login.html", "/").permitAll().anyRequest()
+		.authenticated().and()
+		.addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class)
+		.csrf().csrfTokenRepository(csrfTokenRepository());
+	}
+	private CsrfTokenRepository csrfTokenRepository() {
+		HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+		repository.setHeaderName("X-XSRF-TOKEN");
+		return repository;
+	}
+}
---
+++ src/test/java/com/worksap/SalespusherApplicationTests.java
@@ -0,0 +1,18 @@
+package com.worksap;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SalespusherApplication.class)
+@WebAppConfiguration
+public class SalespusherApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
